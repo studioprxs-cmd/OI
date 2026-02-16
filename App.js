eea806b014e7b3719133d37aaa6a3eb1903bbbf6
studioprxs-cmd <studio.prxs@gmail.com>
2026-02-16 23:18:37 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,16 @@
-import { Text, View } from 'react-native';
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#ffffff',
+      }}
+    >
+      <Text style={{ fontSize: 24, fontWeight: '700' }}>OI deploy test ✅</Text>
+    </View>
+  );
+}
